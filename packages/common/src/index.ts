5de1af5d506678a2d23d1ad5737b1ae430aa0cb2
Abhiraj Thakur <abhirajthakur124@gmail.com>
2024-04-09 11:41:14 +0530

    added transfer and direct-transfer schema

--- packages/common/src/index.ts
+++ packages/common/src/index.ts
@@ -10,3 +10,13 @@ export const RegisterSchema = z.object({
   password: z.string().min(6, { message: "Minimum 6 characters required" }),
   name: z.string().min(1, { message: "Name is required" }),
 });
+
+export const TransferSchema = z.object({
+  amount: z.string().min(1, { message: "Amount cannot be zero" }),
+  bank: z.string().min(1, { message: "Please select a bank provider" }),
+});
+
+export const DirectTransferSchema = z.object({
+  email: z.string().email({ message: "Email is required" }),
+  amount: z.string().min(1, { message: "Amount cannot be zero" }),
+});
