84e4218de829ab47469eb2dc9e5cb68fc08d653f
Abhiraj Thakur <abhirajthakur124@gmail.com>
2024-04-09 11:41:52 +0530

    updated dashboard gretting message

--- apps/client/app/(protected)/dashboard/page.tsx
+++ apps/client/app/(protected)/dashboard/page.tsx
@@ -8,12 +8,14 @@ const greetingText = (): string => {
   const now = moment();
   const currentHour = now.local().hour();
 
-  if (currentHour >= 12 && currentHour <= 16) {
+  if (currentHour < 12) {
+    return "Good Morning";
+  } else if (currentHour < 17) {
     return "Good Afternoon";
-  } else if (currentHour <= 23) {
+  } else if (currentHour < 21) {
     return "Good Evening";
   } else {
-    return "Good Morning";
+    return "Good Night";
   }
 };
 
@@ -101,9 +103,9 @@ function UserIcon(props: { className: string }) {
       fill="currentColor"
     >
       <path
-        fill-rule="evenodd"
+        fillRule="evenodd"
         d="M18.685 19.097A9.723 9.723 0 0 0 21.75 12c0-5.385-4.365-9.75-9.75-9.75S2.25 6.615 2.25 12a9.723 9.723 0 0 0 3.065 7.097A9.716 9.716 0 0 0 12 21.75a9.716 9.716 0 0 0 6.685-2.653Zm-12.54-1.285A7.486 7.486 0 0 1 12 15a7.486 7.486 0 0 1 5.855 2.812A8.224 8.224 0 0 1 12 20.25a8.224 8.224 0 0 1-5.855-2.438ZM15.75 9a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z"
-        clip-rule="evenodd"
+        clipRule="evenodd"
       />
     </svg>
   );
